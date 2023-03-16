273f04326121a154d699afadd0a04a2818ccba88
jibon777 <el_emperatore@yahoo.com>
2023-03-16 13:55:40 +0700

    change port on docker and app

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,6 @@ RUN npm install
 
 COPY --chown=node:node . .
 
-EXPOSE 8081
+EXPOSE 8080
 
 CMD [ "node", "app.js" ]
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const router = express.Router();
 
 const path = __dirname + '/views/';
-const port = 8081;
+const port = 8080;
 
 router.use(function (req,res,next) {
   console.log('/' + req.method);
@@ -22,5 +22,5 @@ app.use(express.static(path));
 app.use('/', router);
 
 app.listen(port, function () {
-  console.log('Example app listening on port 8081!')
+  console.log('Example app listening on port 8080!')
 })
